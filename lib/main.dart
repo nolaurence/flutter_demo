513f0e54dd32b2f6de56929450522d285075689f
nolaurence <nolaurence@outlook.com>
2024-08-13 22:57:32 +0800

    finish demo

--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,31 @@ import 'package:flutter/material.dart';
 class MyAppBar extends StatelessWidget {
   const MyAppBar({required this.title, super.key});
 
-  // Fileds in a weiget subclass are always marked "final".
+  // Fields in a Widget subclass are always marked "final".
 
   final Widget title;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 56,
+      height: 56, // in logical pixels
       padding: const EdgeInsets.symmetric(horizontal: 8),
       decoration: BoxDecoration(color: Colors.blue[500]),
       // Row is a horizontal, linear layout.
       child: Row(
         children: [
           const IconButton(
-            icon: Icon(Icon.menu),
+            icon: Icon(Icons.menu),
             tooltip: 'Navigation menu',
-            onPressed: null,
+            onPressed: null, // null disables the button
           ),
-          // Expanded expands its child to fill the available space
+          // Expanded expands its child
+          // to fill the available space.
           Expanded(
             child: title,
           ),
           const IconButton(
-            icon: Icon(const Icon.search),
+            icon: Icon(Icons.search),
             tooltip: 'Search',
             onPressed: null,
           ),
@@ -36,22 +37,29 @@ class MyAppBar extends StatelessWidget {
   }
 }
 
-class MyScafford extends StatelessWidget {
-  const MyScafford({super.key});
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Material is a conceptual piece of paper on which the UI appears.
+    // Material is a conceptual piece
+    // of paper on which the UI appears.
     return Material(
       // Column is a vertical, linear layout.
       child: Column(
         children: [
           MyAppBar(
-            title: Text('Example title',
-                style: Theme.of(context).primaryTextTheme.titleLarge),
+            title: Text(
+              'Example title',
+              style: Theme.of(context) //
+                  .primaryTextTheme
+                  .titleLarge,
+            ),
           ),
           const Expanded(
-            child: Text('Hello, world!'),
+            child: Center(
+              child: Text('Hello, world!'),
+            ),
           ),
         ],
       ),
@@ -62,9 +70,9 @@ class MyScafford extends StatelessWidget {
 void main() {
   runApp(
     const MaterialApp(
-      title: "My app",
+      title: 'My app', // used by the OS task switcher
       home: SafeArea(
-        child: MyScafford(),
+        child: MyScaffold(),
       ),
     ),
   );
